db1976273a8b5d8a54c10282f62b6827637db44d
FlamyName <gvlad3655@gmail.com>
2026-03-14 14:28:08 +0400

    Добавление логики в модель страницы определенного товара

--- PlantStore/Pages/Plant.cshtml.cs
+++ PlantStore/Pages/Plant.cshtml.cs
@@ -1,12 +1,44 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlantStore.Core.Features.Queries;
+using PlantStore.ViewModels;
 
 namespace PlantStore.Pages
 {
     public class PlantModel : PageModel
     {
-        public void OnGet()
+        private readonly IMediator _mediator;
+        private readonly ILogger<PlantModel> _logger;
+        
+        public ProductIdViewModel? Product { get; set; }
+        
+        public PlantModel(IMediator mediator, ILogger<PlantModel> logger)
         {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            try
+            {
+                Product = await _mediator.Send(new GetProductIdQuery
+                {
+                    Id = id
+                });
+
+                if (Product == null)
+                {
+                    return NotFound();
+                }
+
+                return Page();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
         }
     }
 }

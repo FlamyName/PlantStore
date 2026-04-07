aa9040ee9e33214c706f51084e574b14cd9aa15d
FlamyName <gvlad3655@gmail.com>
2026-04-07 15:58:00 +0400

    Добавление ViewModel и enum для уведомлений

---
+++ BusinessLogic/Core/Notification/Extensions/NotificationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Core.Notification.Extensions
+{
+    public static class NotificationExtensions
+    {
+        public static string ToNotificationType(this NotificationType type)
+        {
+            return type switch
+            {
+                NotificationType.Success => "success",
+                NotificationType.Error => "error",
+                NotificationType.Warning => "warning",
+                NotificationType.Info => "info",
+                _ => "info",
+            };
+        }
+    }
+}
